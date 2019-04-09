e50cf142e015243aee0ffd83c31588d9aac7cc70
hansjeremia <38339876+hansjeremia@users.noreply.github.com>
2019-04-09 16:46:12 +0700

    main.c

--- main.c
+++ main.c
@@ -2,21 +2,58 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <string.h>
-#include "Load.c"
-#include "Load.h"
+#include "Lib.c"
+#include "Lib.h"
+
+typedef struct checker {
+    int minggu [2];
+    char *hari [2];
+} Checker;
 
 //Variabel
 int menu;
 int MenuUtama;
 char NamaProyek[100];
-int RC[12][7];
+Checker checker;
+Schedule jadwal;
 
 //Fungsi & Prosedur
 void menuUtama();
-void RuleChecker();
+void RuleChecker(Checker checker, Schedule jadwal);
+void buildChecker(Checker *checker);
 
 //Main Program
 int main(){
+
+    //isi schedule
+    char const * day;
+    jadwal.week[0].element= 3;
+    day = "Rabu";
+    //alokasi memori
+    jadwal.week[0].day[2].element = malloc((strlen(day))+1);
+    jadwal.week[0].day[2].element=  day;
+
+    day = "Senin";
+    for (int i = 0 ; i < 5; i++){
+        if (i != 2){
+           //alokasi memori
+        jadwal.week[0].day[i].element = malloc((strlen(day))+1);
+        jadwal.week[0].day[i].element=  day;
+        }
+    }
+    jadwal.week[1].element= 4;
+    day = "Senin";
+    //alokasi memori
+    jadwal.week[1].day[0].element = malloc((strlen(day))+1);
+    jadwal.week[1].day[0].element=  day;
+
+    day = "Rabu";
+    for (int i = 1 ; i < 5; i++){
+         //alokasi memori
+        jadwal.week[1].day[i].element = malloc((strlen(day))+1);
+        jadwal.week[1].day[i].element=  day;
+    }
+
     printf("Menu : \n");
     printf("\t 1. Buat Proyek Baru\n");
     printf("\t 2. Buat Proyek dari Berkas\n");
@@ -69,16 +106,56 @@ void menuUtama(){
     }else if(MenuUtama==6){
 
     }else if(MenuUtama==7){
+        buildChecker(&checker);
+        RuleChecker(checker, jadwal);
 
     }else if(MenuUtama==8){
 
     }
 }
 
-void RuleChecker(){
-    int i,j;
-    for (i=0;i<12;i++){
-        
+void buildChecker(Checker *checker){
+    char const *hari;
+    //assign buat contoh saja
+    checker ->minggu[0] = 3;
+    hari = "Rabu";
+    checker->hari[0] = malloc(strlen(hari)+1);
+    strcpy(checker->hari[0],hari);
+
+
+    checker ->minggu[1] = 4;
+    hari = "Senin";
+    checker->hari[1] = malloc(strlen(hari)+1);
+    strcpy(checker->hari[1],hari);
+
+}
+void RuleChecker(Checker checker, Schedule jadwal){
+
+    int i_minggu;
+    int i_hari;
+    for (int i = 0; i<size;i++){
+        printf("minggu : %d Hari : %s\n", checker.minggu[i], checker.hari[i]);
+         i_minggu = 0;
+         i_hari = 0;
+
+        //untuk database index pertama
+        while ((checker.minggu[i] != jadwal.week[i_minggu].element)&&(i_minggu <2)){
+            i_minggu++;
+        }
+
+        //cek hari
+        while ((strcmp(checker.hari[i],jadwal.week[i_minggu].day[i_hari].element)!=0)&&(i_hari <5)){
+
+
+//        printf("%s : %s\n", checker.hari[i],jadwal.week[i_minggu].day[i_hari].element);
+//        printf("%d : %d\n", strlen(checker.hari[i]),strlen(jadwal.week[i_minggu].day[i_hari].element));
+
+        i_hari++;
+
+        }
+
+        printf("i_minggu: %d i_hari : %d\n", i_minggu,i_hari);
     }
+
 
 }
